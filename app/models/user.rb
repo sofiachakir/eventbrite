e40b2a3c72aa9123e36876b80733227308c7024a
Reivnaj <pelletierjanvier@gmail.com>
2020-05-11 18:02:55 +0200

    setting action mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   has_many :administrated_events, foreign_key: "admin_id", class_name: "Event"
   has_many :attendances
   has_many :events, through: :attendances
-  
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
