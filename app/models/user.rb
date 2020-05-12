be93270559e7c574e24f7eadc14cb11dfde21e24
Reivnaj <pelletierjanvier@gmail.com>
2020-05-12 15:39:46 +0200

    fucking mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   after_create :welcome_send
   has_many :administarted_events, foreign_key: "admin_id", class_name: "Event"
   has_many :attendances
