f93d948420ab749d06d7f54f6055461fd1e4b47a
Reivnaj <pelletierjanvier@gmail.com>
2020-05-14 17:16:29 +0200

    correct routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'events#index'
-<<<<<<< HEAD
-  resources :events, only: [:new, :create, :show] do
-  	resources :attendances, only: [:new, :create]
-    resources :event_picture, only: [:create]
-=======
   resources :events do
   	resources :attendances, only: [:new, :create, :index]
->>>>>>> d6bddb722ed1af8931fa908a18504fc9bcb70063
+    resources :event_picture, only: [:create]
   end
   resources :users, only: [:show]
 
